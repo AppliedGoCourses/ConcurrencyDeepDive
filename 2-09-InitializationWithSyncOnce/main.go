121e41ae3368741ee15098b7d4bc1b663ab4aaba
christophberger <christophberger@users.noreply.github.com>
2022-02-06 14:11:39 +0100

    feat: remove path argument for simplicity chore: decrease indent depth chore: print the name of the function used

--- 2-09-InitializationWithSyncOnce/main.go
+++ 2-09-InitializationWithSyncOnce/main.go
@@ -17,37 +17,38 @@ type FileBuffer struct {
 // BadGetFile loads a file if not already cached,
 // and returns the file contents as a byte slice.
 // When called concurrently, it may load the same file multiple times.
-func (f *FileBuffer) BadGetFile(path string) []byte {
+func (f *FileBuffer) BadGetFile() []byte {
 	if f == nil {
 		log.Fatalln("receiver must not be nil")
 	}
-	if f.data == nil {
-		// A very simple way to demonstrate that BadGetFile()
-		// is not safe for concurrent use.
-		fmt.Println("loading", path)
+	if f.data != nil {
+		return f.data
+	}
+	// A very simple way to demonstrate that BadGetFile()
+	// is not safe for concurrent use.
+	fmt.Println("BadGetFile: loading 'data'")
 
-		var err error
-		f.data, err = os.ReadFile(path)
-		// quick  & dirty error handling for brevity
-		if err != nil {
-			log.Fatalln(err)
-			return nil
-		}
+	var err error
+	f.data, err = os.ReadFile("data")
+	// quick  & dirty error handling for brevity
+	if err != nil {
+		log.Fatalln(err)
+		return nil
 	}
 	return f.data
 }
 
 // GoodGetFile uses a sync.Once object to ensure that the file is loaded
 // only once.
-func (f *FileBuffer) GoodGetFile(path string) []byte {
+func (f *FileBuffer) GoodGetFile() []byte {
 	if f == nil {
 		log.Fatalln("receiver must not be nil")
 	}
 	f.once.Do(func() {
-		fmt.Println("loading", path)
+		fmt.Println("GoodGetFile: loading 'data'")
 
 		var err error
-		f.data, err = os.ReadFile(path)
+		f.data, err = os.ReadFile("data")
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -62,8 +63,8 @@ func main() {
 	for i := 1; i < 1000; i++ {
 		wg.Add(1)
 		go func() {
-			// Test this call with BadGetFile() and GoodGetFile().
-			data := b.GoodGetFile("data")
+			// Test this call with both BadGetFile() and GoodGetFile().
+			data := b.BadGetFile()
 			if data == nil {
 				log.Println("error: file not loaded")
 			}

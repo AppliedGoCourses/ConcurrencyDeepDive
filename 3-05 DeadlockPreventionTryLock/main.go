1744be1e22f1585144f7c217a6b3322e07c67d4b
christophberger <christophberger@users.noreply.github.com>
2022-02-27 13:54:02 +0100

    ui: improve log output

--- 3-05 DeadlockPreventionTryLock/main.go
+++ 3-05 DeadlockPreventionTryLock/main.go
@@ -17,26 +17,29 @@ func copyFileTryLock(task string, source, target *File) {
 	// get exclusive access to both files
 	var lock1 bool
 	for {
+		log.Printf("%s: trying to lock both files\n", task)
 		if source.mu.TryLock() {
-			log.Printf("%s: source %s is locked\n", task, source.path)
+			log.Printf("%s: source '%s' is locked\n", task, source.path)
 			lock1 = true
 
-			// simulate time for opening the source file
-			// to highly increase the chances for a deadlock
+			// for testing purposes, add a delay
+			// to highly increase the chances to observe a deadlock
 			time.Sleep(time.Millisecond)
 
 			if target.mu.TryLock() {
-				log.Printf("%s: target %s is locked\n", task, target.path)
+				log.Printf("%s: target '%s' is locked\n", task, target.path)
 				break
 			}
 		}
 		// At this point, at least one of the locks is still not acquired.
+		log.Printf("%s: could not acquire both locks\n", task)
 		// Unlock the source mutex if necessary, back off for a while, then try again
 		if lock1 {
-			log.Printf("%s: unlock source %s and back off\n", task, source.path)
+			log.Printf("%s: unlock source '%s'\n", task, source.path)
 			source.mu.Unlock()
 			lock1 = false
 		}
+		log.Printf("%s: back off\n", task)
 		time.Sleep(time.Millisecond)
 	}
 
@@ -45,9 +48,9 @@ func copyFileTryLock(task string, source, target *File) {
 	copy(target.data[:], source.data[:])
 
 	// release the file locks again
-	log.Printf("%s: unlock source %s\n", task, source.path)
+	log.Printf("%s: unlock source '%s'\n", task, source.path)
 	target.mu.Unlock()
-	log.Printf("%s: unlock target %s\n", task, target.path)
+	log.Printf("%s: unlock target '%s'\n", task, target.path)
 	source.mu.Unlock()
 }
 
